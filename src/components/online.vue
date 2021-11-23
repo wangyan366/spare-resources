5f38f28be0256b1c3a2df66637f749a1bc36a774
wangyan <12345678>
2021-11-23 20:55:04 +0800

    添加图片

--- src/components/online.vue
+++ src/components/online.vue
@@ -1,41 +1,46 @@
 
 <template>
-  <div class="hello">
-    <h1>首页</h1>
-  </div>
+	<div class="online">
+		<van-image :src="img" />
+	</div>
 </template>
 
 <script>
+import {
+
+	Image as VanImage,
+
+} from "vant";
 export default {
-  name: "HelloWorld",
-  data() {
-    return {
-      count: "", //倒计时
-    };
-  },
-  mounted() {
-    setTimeout(() => {
-      this.$router.push({ path: "/home" }); // 强制切换当前路由 path
-    }, 3000);
-  },
+	name: "Online",
+	components: {
+		[VanImage.name]: VanImage,
+	},
+	data() {
+		return {
+			img: require("@/assets/images/header.png")
+		};
+	},
+	mounted() {
+		// setTimeout(() => {
+		// 	this.$router.push({ path: "/home" }); // 强制切换当前路由 path
+		// }, 3000);
+	},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
+.online{
+  width: 100%;
+  height: 100%;
+  position: relative;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.deep/ .van-image{
+    position: absolute;
+  top:50%;
+  left:50%;
+  transform:translate(-50%,-50%);
+  text-align: center;
 }
 </style>
